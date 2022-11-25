19c4fe0db38b3ee026fcf8b64c85f66e94b4c7aa
FenteMontoto <fentemontoto@gmail.com>
2022-11-25 10:46:57 +0100

    Creación y configuración base de datos mysql

---
+++ bi_lms/Readme.md
@@ -0,0 +1,17 @@
+# Confirguar BBDD
+
+## Instalar mysqlclientn el directorio del proyecto
+
+### pip install mysqlclient
+
+## Iniciar sesion en mysql  en cmd
+
+### mysql -u root -p
+
+## Crear BBDD
+
+### CREATE DATABASE bd_bi_lms
+
+## Usar BBDD
+
+### USE bd_bi_lms
--- bi_lms/bi_lms/settings.py
+++ bi_lms/bi_lms/settings.py
@@ -84,8 +84,14 @@ WSGI_APPLICATION = "bi_lms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'bd_bi_lms',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '3306',
     }
 }
 
